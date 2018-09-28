89e9086ee0f3c6d6aa5d625f0ab98f671e182787
Pawel Debinski <spokojasne@interia.eu>
2018-09-28 09:08:24 +0200

    added rest updateReservation, closes #17

--- src/main/java/pl/pawel/flightreservation/controllers/ReservationRestController.java
+++ src/main/java/pl/pawel/flightreservation/controllers/ReservationRestController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.pawel.flightreservation.dto.ReservationUpdateRequest;
 import pl.pawel.flightreservation.entities.Reservation;
 import pl.pawel.flightreservation.repos.ReservationRepository;
 
@@ -16,5 +17,13 @@ public class ReservationRestController {
     @RequestMapping("/reservations/{id}")
     public Reservation findReservation(@PathVariable("id") Long id) {
         return reservationRepository.findById(id).get();
+    }
+
+    @RequestMapping("/reservations")
+    public Reservation updateReservation(ReservationUpdateRequest request) {
+        Reservation reservation = reservationRepository.findById(request.getId()).get();
+        reservation.setNumberOfBags(request.getNumberOfBags());
+        reservation.setCheckedIn(request.getCheckedIn());
+        return reservationRepository.save(reservation);
     }
 }
