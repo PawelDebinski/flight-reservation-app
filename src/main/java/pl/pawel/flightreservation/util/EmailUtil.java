b9e132c08274606944478674171e63b0cf955038
Pawel Debinski <spokojasne@interia.eu>
2018-09-28 18:24:13 +0200

    externalized properties, closes #29

--- src/main/java/pl/pawel/flightreservation/util/EmailUtil.java
+++ src/main/java/pl/pawel/flightreservation/util/EmailUtil.java
@@ -3,6 +3,8 @@ package pl.pawel.flightreservation.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
@@ -12,12 +14,22 @@ import javax.mail.internet.MimeMessage;
 import java.io.File;
 
 @Component
+@PropertySource("classpath:myconfig.properties")
 public class EmailUtil {
-
+    
     @Autowired
     private JavaMailSender sender;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EmailUtil.class);
+
+    @Value("${pl.pawel.flightreservation.itinerary.email.subject}")
+    private String EMAIL_SUBJECT;
+
+    @Value("${pl.pawel.flightreservation.itinerary.email.body}")
+    private String EMAIL_BODY;
+
+    @Value("${pl.pawel.flightreservation.itinerary.email.sender}")
+    private String EMAIL_SENDER;
 
     public void sendItinerary(String toAddress, String filePath) {
         LOGGER.info("=== Inside sendItinerary() -> toAddress: {}, filePath: {}", toAddress, filePath);
@@ -25,10 +37,10 @@ public class EmailUtil {
 
         try {
             MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
-            messageHelper.setFrom("iammarrypoppinsyo@gmail.com");
+            messageHelper.setFrom(EMAIL_SENDER);
             messageHelper.setTo(toAddress);
-            messageHelper.setSubject("Itinerary for your Flight");
-            messageHelper.setText("Please find your Itinerary attached.");
+            messageHelper.setSubject(EMAIL_SUBJECT);
+            messageHelper.setText(EMAIL_BODY);
             messageHelper.addAttachment("Itinerary", new File(filePath));
             sender.send(message);
 
