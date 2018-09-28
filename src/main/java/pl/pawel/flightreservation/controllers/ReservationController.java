a1836d092e253ece3d54f1b6a427427e780fa28b
Pawel Debinski <spokojasne@interia.eu>
2018-09-28 07:59:14 +0200

    added reservation confirmation view, closes #15

--- src/main/java/pl/pawel/flightreservation/controllers/ReservationController.java
+++ src/main/java/pl/pawel/flightreservation/controllers/ReservationController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import pl.pawel.flightreservation.dto.ReservationRequest;
 import pl.pawel.flightreservation.entities.Flight;
+import pl.pawel.flightreservation.entities.Reservation;
 import pl.pawel.flightreservation.repos.FlightRepository;
+import pl.pawel.flightreservation.services.ReservationService;
 
 
 @Controller
@@ -16,6 +18,9 @@ public class ReservationController {
 
     @Autowired
     FlightRepository flightRepository;
+
+    @Autowired
+    ReservationService reservationService;
 
     @RequestMapping("/showCompleteReservation")
     public String showCompleteReservation(@RequestParam("flightId") Long flightId, ModelMap modelMap) {
@@ -25,8 +30,9 @@ public class ReservationController {
     }
 
     @PostMapping("/completeReservation")
-    public String completeReservation(ReservationRequest request) {
-
-        return null;
+    public String completeReservation(ReservationRequest request, ModelMap modelMap) {
+        Reservation reservation = reservationService.bookFlight(request);
+        modelMap.addAttribute("msg", "Reservation created successfully and id is " + reservation.getId());
+        return "reservationConfirmation";
     }
 }
